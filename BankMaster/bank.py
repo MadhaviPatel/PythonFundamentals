928609e44dc65bcda76eb747b0163e83f5d2cc10
Madhavi Patel <madhavipatel@Madhavis-MacBook-Air.local>
2020-06-04 20:43:17 +0530

    create main menu

---
+++ BankMaster/bank.py
@@ -0,0 +1,8 @@
+print("***** Bank Managment *****")
+
+bank_data = {}
+
+while True:
+    print("\n----- Main Menu -----")
+    ch = int(input("1.New Customer\n2.Existing Customer\n3.Exit\n\nEnter choice:"))
+
