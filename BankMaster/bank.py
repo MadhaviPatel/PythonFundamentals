933f0ec72e2825ff1760af652169d40317d2fe19
Madhavi Patel <madhavipatel@Madhavis-MacBook-Air.local>
2020-06-05 11:35:13 +0530

    bank functionality

--- BankMaster/bank.py
+++
@@ -1,8 +0,0 @@
-print("***** Bank Managment *****")
-
-bank_data = {}
-
-while True:
-    print("\n----- Main Menu -----")
-    ch = int(input("1.New Customer\n2.Existing Customer\n3.Exit\n\nEnter choice:"))
-
